cfeec472bc2cf1f071515d89fecd35f256cd5274
Kaizan Haque <kaizanhaque@gmail.com>
2025-07-08 23:13:23 -0500

    some refactoring of message handling, started reaction handler and react4role response message but need to combine them

--- main.go
+++ main.go
@@ -5,22 +5,45 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/bwmarrin/discordgo"
 )
 
-func petCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.Bot {
+// reads all messages and dispatches the appropriate response 
+// hope this doesn't skyrocket my AWS bill D:
+func newMessage(session *discordgo.Session, message *discordgo.MessageCreate) {
+	// Ignore bot messaage
+	if message.Author.ID == session.State.User.ID {
 		return
 	}
 
-	if m.Content == "!pet" {
-		s.ChannelMessageSend(m.ChannelID, "*hapy capy noises*")
+	// Respond to messages
+	switch {
+	case strings.Contains(message.Content, "!react4roles"):
+		if message.Content[0:12] != "!react4roles" { break } //command appeared somewhere besides the front
+		react4rolesCommand(session, message)
+	case message.Content == "!pet" :
+		session.ChannelMessageSend(message.ChannelID, "*hapy capy noises*")
 	}
 }
 
+//Command: !react4roles <role name>
+func react4rolesCommand(session *discordgo.Session, message *discordgo.MessageCreate) {
+	// get the name of the role
+	roleName := message.Content[12:]
+	session.ChannelMessageSend(message.ChannelID, "React for role: " + roleName)
+	fmt.Println(session.MessageReactions)
+}
+
+// TODO: handle reactions to the react4roles bot response message
+func react4rolesReactionHandler(session *discordgo.Session, reaction *discordgo.MessageReactionAdd) {
+	fmt.Println(reaction.UserID)
+}
+
 func main() {
+	fmt.Println("Loading .env")
 	// need the token to get the client for the bot
 	err := godotenv.Load()
 	if err != nil {
@@ -33,8 +56,9 @@ func main() {
 	// make a client
 	client, err := discordgo.New("Bot " + discordToken)
 
-	// add your handlers (keeping one per command for cleanliness)
-	client.AddHandler(petCommand)
+	// add your handlers (newMessage takes care of commands and deploys the correct response/response functions
+	client.AddHandler(newMessage)
+	client.AddHandler(react4rolesReactionHandler)
 
 	// open the client
 	err = client.Open()
