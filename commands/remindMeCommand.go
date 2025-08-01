52bd32857120f9d9ebc022da617f3a94a95b75cb
= <kaizanhaque@gmail.com>
2025-07-31 20:42:29 -0500

    added message capability to remindMe command

--- commands/remindMeCommand.go
+++ commands/remindMeCommand.go
@@ -12,49 +12,67 @@ func RemindMeCommand(session *discordgo.Session, message *discordgo.MessageCreat
 	splits := strings.Split(message.Content[10:], ":")
 
 	if len(splits) > 3 {
-		session.ChannelMessageSend(message.ChannelID, "Error: use for !remindMe is <days>:<hours>:<minutes>")
+		session.ChannelMessageSend(message.ChannelID, "2Error: use for !remindMe is <days>:<hours>:<minutes>")
 		return
 	}
 
 	// var days, hours, minutes string = "", "", ""
 	_, err := strconv.Atoi(splits[0])
 	if err != nil { 	// we check that we were given a proper number by trying to convert to an int
-		session.ChannelMessageSend(message.ChannelID, "Error: failed to parse number of days from input, are you sure it's an integer?")
+		session.ChannelMessageSend(message.ChannelID, "2Error: failed to parse number of days from input, are you sure it's an integer?")
 			return
 	}
 	if len(splits) > 1 {
 		_, err := strconv.Atoi(splits[1])
 		if err != nil {
-			session.ChannelMessageSend(message.ChannelID, "Error: failed to parse number of hours from input, are you sure it's an integer?")
+			session.ChannelMessageSend(message.ChannelID, "2Error: failed to parse number of hours from input, are you sure it's an integer?")
 			return
 		}
 	}
 	if len(splits) > 2 {
-		_, err := strconv.Atoi(splits[2])
+		_, err := strconv.Atoi(strings.Split(splits[2], " ")[0])
 		if err != nil {
-			session.ChannelMessageSend(message.ChannelID, "Error: failed to parse number of minutes from input, are you sure it's an integer?")
+			session.ChannelMessageSend(message.ChannelID, "2Error: failed to parse number of minutes from input, are you sure it's an integer?")
 			return
 		}
 	}
-	go remind(session, message, splits[0], splits[1], splits[2])
+
+	// user can add a message with the reminder so we cut off the minutes reading at the space character if needed
+	if len(strings.Split(splits[2], " ")) > 1 {
+		go remind(session, message)
+	} else {
+		go remind(session, message)
+	}
 }
 
 // helper function called as goroutine that actually handles the reminding
-func remind(session *discordgo.Session, message *discordgo.MessageCreate, days string, hours string, minutes string) {
+func remind(session *discordgo.Session, message *discordgo.MessageCreate) {
+	splits := strings.Split(message.Content[10:], ":")
+	days := splits[0]
+	hours := splits[1]
+	minutes := ""
+	msg := ""
+	if len(strings.Split(splits[2], " ")) > 1 { // if there's a message, take only the minutes part
+		minutes = strings.Split(splits[2], " ")[0]
+		msg = strings.Split(splits[2], " ")[1]
+	} else {	// if no message, take from the second colon to the end (days:hours:minutes)
+		minutes = splits[2]
+	}
+
 	intDays, err := strconv.Atoi(days)
 	if err != nil { // I don't think these errors should trigger because we checked earlier, but Go wants me to
-		session.ChannelMessageSend(message.ChannelID, "Error: couldn't parse days from given input in timer goroutine")
+		session.ChannelMessageSend(message.ChannelID, "2Error: couldn't parse days from given input in timer goroutine")
 	}
 	intHours, err := strconv.Atoi(hours)
 	if err != nil { 
-		session.ChannelMessageSend(message.ChannelID, "Error: couldn't parse hours from given input in timer goroutine")
+		session.ChannelMessageSend(message.ChannelID, "2Error: couldn't parse hours from given input in timer goroutine")
 	}
 
 	sleepHours := intDays * 24 + intHours // time.ParseDuration() doesn't parse days :(
 
 	duration, err := time.ParseDuration(strconv.Itoa(sleepHours) + "h" + minutes + "m")
 	if err != nil {
-		session.ChannelMessageSend(message.ChannelID, "Error: couldn't parse time from given input in timer goroutine")
+		session.ChannelMessageSend(message.ChannelID, "2Error: couldn't parse time from given input in timer goroutine")
 		return
 	}
 
@@ -71,7 +89,7 @@ func remind(session *discordgo.Session, message *discordgo.MessageCreate, days s
 	time.Sleep(time.Minute * time.Duration(duration.Minutes()))
 
 	session.ChannelMessageSendComplex(message.ChannelID, &discordgo.MessageSend {
-		Content: "Time's up!",
+		Content: "Time's up! " + msg,
 		Reference: &discordgo.MessageReference {
 			MessageID: message.ID,
 			ChannelID: message.ChannelID,
