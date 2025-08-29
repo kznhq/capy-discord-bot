3d853c9a91204eb414c8286168b2d1a6ce9a5758
= <kaizanhaque@gmail.com>
2025-08-29 00:58:04 -0500

    fixed issue where capy was trying to do role stuff when any reaction was done, changed it now so it's only when reactions are added/deleted to the react4role message from capy

--- handlers/react4RoleReactionAddHandler.go
+++ handlers/react4RoleReactionAddHandler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"strings"
+
 	"github.com/kznhq/capyDiscordBot/utils"
 
 	"github.com/bwmarrin/discordgo"
@@ -8,12 +10,29 @@ import (
 
 // called when someone reacts to the bot's message in order to get a role
 func React4roleReactionAddHandler(session *discordgo.Session, reaction *discordgo.MessageReactionAdd) {
+	// first we check to see that the reaction was made to a react4role message
+	msg, err := session.ChannelMessage(reaction.ChannelID, reaction.MessageID)
+	if err != nil {
+		session.ChannelMessageSend(reaction.ChannelID, "Error when checking reacted message")
+		return
+	}
+
+	// if the message reacted to wasn't from the bot
+	if msg.Author.ID != session.State.User.ID {
+		return
+	}
+
+	// we make sure that the reaction was done to a react4role message
+	if !(strings.Contains(msg.Content, "React for role: ") && msg.Content[0:16] == "React for role: ") { 
+		return
+	}
+
 	var roleId string
 
 	// get the role ID from the db that corresponds to the message ID of the message that was reacted to
 	row := utils.GetRoleFromMsgStatement.QueryRow(reaction.MessageID)
 
-	err := row.Scan(&roleId)
+	err = row.Scan(&roleId)
 	if err != nil {
 		session.ChannelMessageSend(reaction.ChannelID, "Error when getting result from database")
 		return
